0e15330741fbf9aab4e0b6c0cb90d215923a09da
Jennifer Parks <jp00213@my.westga.edu>
2022-04-26 17:36:42 -0400

    Adds Gradebook variable for total points. Adds grade-averaging methods.

--- src/edu/westga/cs6312/grades/model/Gradebook.java
+++ src/edu/westga/cs6312/grades/model/Gradebook.java
@@ -15,16 +15,42 @@ import java.util.ArrayList;
  */
 public class Gradebook {
 	private ArrayList<Grades> studentGradebook;
+	private Grades potentialPoints;
 
 	/**
 	 * 0-parameter constructor, initializes object
 	 */
 	public Gradebook() {
 		this.studentGradebook = new ArrayList<Grades>();
+		this.potentialPoints = new Grades();
 	}
 
 	/**
-	 * Adds a new Grades object to the ArrayList
+	 * Sets a Grades object as the maximum value of potential points for all graded
+	 * elements
+	 * 
+	 * @param newGrades Grades object to set as maximum potential points
+	 * @precondition Grades cannot be null
+	 */
+	public void addToPotentialPoints(Grades newGrades) {
+		if (newGrades == null) {
+			throw new IllegalArgumentException("Cannot add null grades");
+		}
+		this.potentialPoints = newGrades;
+	}
+
+	/**
+	 * Gets Grades object of maximum potential points
+	 * 
+	 * @return Grades collection of maximum potential points
+	 */
+	public Grades getPotentialPoints() {
+		return this.potentialPoints;
+	}
+
+	/**
+	 * Adds a new Grades object to the student ArrayList (represents one student's
+	 * scores)
 	 * 
 	 * @param newGrades Grades object to add to ArrayList
 	 * @precondition Grades cannot be null
@@ -43,5 +69,47 @@ public class Gradebook {
 	 */
 	public ArrayList<Grades> getGradebookData() {
 		return this.studentGradebook;
+	}
+
+	/**
+	 * Calculates average lab grades for each student in Gradebook
+	 * 
+	 * @return ArrayList of all average lab grades (by student)
+	 */
+	public ArrayList<Double> getAverageGradesLab() {
+		ArrayList<Double> labAverages = new ArrayList<Double>();
+		for (Grades element : this.studentGradebook) {
+			double average = ((double) element.sumGradesLab() / this.potentialPoints.sumGradesLab()) * 100;
+			labAverages.add(average);
+		}
+		return labAverages;
+	}
+
+	/**
+	 * Calculates average project grades for each student in Gradebook
+	 * 
+	 * @return ArrayList of all average project grades (by student)
+	 */
+	public ArrayList<Double> getAverageGradesProject() {
+		ArrayList<Double> projectAverages = new ArrayList<Double>();
+		for (Grades element : this.studentGradebook) {
+			double average = ((double) element.sumGradesProject() / this.potentialPoints.sumGradesProject()) * 100;
+			projectAverages.add(average);
+		}
+		return projectAverages;
+	}
+
+	/**
+	 * Calculates average test grades for each student in Gradebook
+	 * 
+	 * @return ArrayList of all average test grades (by student)
+	 */
+	public ArrayList<Double> getAverageGradesTest() {
+		ArrayList<Double> testAverages = new ArrayList<Double>();
+		for (Grades element : this.studentGradebook) {
+			double average = ((double) element.sumGradesTest() / this.potentialPoints.sumGradesTest()) * 100;
+			testAverages.add(average);
+		}
+		return testAverages;
 	}
 }
--- src/edu/westga/cs6312/grades/tests/GradebookWhenCreateGradebook.java
+++ src/edu/westga/cs6312/grades/tests/GradebookWhenCreateGradebook.java
@@ -23,16 +23,94 @@ import edu.westga.cs6312.grades.model.Grades;
 public class GradebookWhenCreateGradebook {
 
 	/**
-	 * Tests creating Gradebook object
+	 * Tests creating Gradebook object, add object to student gradebook
 	 */
 	@Test
-	public void testCreateGradebook() {
+	public void testCreateGradebook1Student() {
 		Gradebook testGradebook = new Gradebook();
 		Grades addGrades1 = new Grades();
 		testGradebook.addToGradebook(addGrades1);
 		ArrayList<Grades> expected = new ArrayList<Grades>();
 		expected.add(addGrades1);
 		ArrayList<Grades> result = testGradebook.getGradebookData();
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Tests creating Gradebook object, add object to potential points
+	 */
+	@Test
+	public void testCreateGradebook1PotentialPoints() {
+		Gradebook testGradebook = new Gradebook();
+		Grades addGrades1 = new Grades();
+		testGradebook.addToPotentialPoints(addGrades1);
+		Grades result = testGradebook.getPotentialPoints();
+		assertEquals(addGrades1, result);
+	}
+
+	/**
+	 * Tests creating Gradebook object, calculate average lab grades
+	 */
+	@Test
+	public void testCreateGradebookGetAverageLab() {
+		Gradebook testGradebook = new Gradebook();
+		
+		Grades addGradesPotential = new Grades();
+		addGradesPotential.setGradesLab(100);
+		addGradesPotential.setGradesLab(100);
+		testGradebook.addToPotentialPoints(addGradesPotential);
+		Grades addGradesStudent = new Grades();
+		addGradesStudent.setGradesLab(90);
+		addGradesStudent.setGradesLab(80);
+		testGradebook.addToGradebook(addGradesStudent);
+		
+		ArrayList<Double> result = testGradebook.getAverageGradesLab();
+		ArrayList<Double> expected = new ArrayList<Double>();
+		expected.add(85.0);
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Tests creating Gradebook object, calculate average project grades
+	 */
+	@Test
+	public void testCreateGradebookGetAverageProject() {
+		Gradebook testGradebook = new Gradebook();
+		
+		Grades addGradesPotential = new Grades();
+		addGradesPotential.setGradesProject(100);
+		addGradesPotential.setGradesProject(100);
+		testGradebook.addToPotentialPoints(addGradesPotential);
+		Grades addGradesStudent = new Grades();
+		addGradesStudent.setGradesProject(70);
+		addGradesStudent.setGradesProject(90);
+		testGradebook.addToGradebook(addGradesStudent);
+		
+		ArrayList<Double> result = testGradebook.getAverageGradesProject();
+		ArrayList<Double> expected = new ArrayList<Double>();
+		expected.add(80.0);
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Tests creating Gradebook object, calculate average test grades
+	 */
+	@Test
+	public void testCreateGradebookGetAverageTest() {
+		Gradebook testGradebook = new Gradebook();
+		
+		Grades addGradesPotential = new Grades();
+		addGradesPotential.setGradesTest(100);
+		addGradesPotential.setGradesTest(100);
+		testGradebook.addToPotentialPoints(addGradesPotential);
+		Grades addGradesStudent = new Grades();
+		addGradesStudent.setGradesTest(50);
+		addGradesStudent.setGradesTest(90);
+		testGradebook.addToGradebook(addGradesStudent);
+		
+		ArrayList<Double> result = testGradebook.getAverageGradesTest();
+		ArrayList<Double> expected = new ArrayList<Double>();
+		expected.add(70.0);
 		assertEquals(expected, result);
 	}
 
